e41a7ea89bc2fdad1a29a2cd66b49af051324839
joseewu <josee@chocolabs.com>
2018-08-01 23:55:17 +0800

    輸給一個XOR operator

--- 2_OddOccurrencesInArray.playground/Contents.swift
+++ 2_OddOccurrencesInArray.playground/Contents.swift
@@ -1,4 +1,23 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
 import Foundation
+
+class AddOccurrencsInArray{
+    init() {
+
+    }
+    public func solution(_ A : inout [Int]) -> Int {
+        let arrayCount:Int = A.count
+        if arrayCount%2 == 0 {
+            return 0
+        }
+        var result:Int = 0
+        for item in A {
+            result ^= item
+        }
+        return result
+    }
+}
+
+var testCase = [1,1,2,2,2,2,2,8,2]
+let result = AddOccurrencsInArray().solution(&testCase)
